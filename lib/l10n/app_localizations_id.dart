d7fc3c2105e227dca7aad24bda8334e405d76d57
yysrll <yusril.uzumaki@gmail.com>
2026-02-28 23:02:26 +0700

    setup l10n

---
+++ lib/l10n/app_localizations_id.dart
@@ -0,0 +1,21 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Indonesian (`id`).
+class AppLocalizationsId extends AppLocalizations {
+  AppLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get welcomeTitle => 'Selamat datang di Kas Rumah';
+
+  @override
+  String get tagline =>
+      'Kelola anggaran keluarga lebih rapi, demi masa depan yang lebih pasti.';
+
+  @override
+  String get taglineAlternative =>
+      'Bocoran pengeluaran terdeteksi, tabungan keluarga makin terkendali.';
+}
