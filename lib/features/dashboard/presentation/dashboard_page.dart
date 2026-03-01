d7744b41c12eb1aac1060aea227e3c4e3a3bf9c0
yysrll <yusril.uzumaki@gmail.com>
2026-03-01 15:00:41 +0700

    Add DashboardPage and navigation from WorkspacePage

---
+++ lib/features/dashboard/presentation/dashboard_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:kas_rumah/components/layouts/app_scaffold.dart';
+import 'package:kas_rumah/core/utils/context/context_ext.dart';
+import 'package:kas_rumah/core/utils/context/shad_context_ext.dart';
+
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _selectedIndex = 0;
+
+  // Daftar halaman untuk setiap tab
+  final List<Widget> _pages = [
+    const Center(child: Text("Home - Ringkasan Kas")),
+    const Center(child: Text("Transaction - Transaksi Terbaru")),
+    const Center(child: Text("Insight - Analisis Pengeluaran")),
+    const Center(child: Text("Budget - Atur Anggaran")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.shadTheme;
+
+    return AppScaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 4,
+        selectedItemColor: theme.colorScheme.primary,
+        unselectedItemColor: theme.colorScheme.mutedForeground,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(LucideIcons.house),
+            label: context.strings.overviewTitle,
+          ),
+
+          BottomNavigationBarItem(
+            icon: const Icon(LucideIcons.handCoins),
+            label: context.strings.transactionsTitle,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(LucideIcons.chartBar),
+            label: context.strings.insightsTitle,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(LucideIcons.wallet),
+            label: context.strings.budgetsTitle,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() => _selectedIndex = index);
+        },
+      ),
+    );
+  }
+}
--- lib/features/workspace/presentation/list/workspace_page.dart
+++ lib/features/workspace/presentation/list/workspace_page.dart
@@ -3,6 +3,7 @@ import 'package:kas_rumah/components/layouts/app_scaffold.dart';
 import 'package:kas_rumah/core/utils/constant/dimen_constant.dart';
 import 'package:kas_rumah/core/utils/context/context_ext.dart';
 import 'package:kas_rumah/core/utils/context/shad_context_ext.dart';
+import 'package:kas_rumah/features/dashboard/presentation/dashboard_page.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 class WorkspacePage extends StatelessWidget {
@@ -20,34 +21,44 @@ class WorkspacePage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: kDefaultScreenPadding),
         child: ListView.separated(
           itemBuilder: (context, index) {
-            return Container(
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                borderRadius: context.shadTheme.radius,
-                border: Border.all(color: context.shadTheme.colorScheme.border),
-              ),
-              child: Row(
-                spacing: 12,
-                children: [
-                  Icon(
-                    LucideIcons.notebookPen,
-                    size: 24,
-                    color: context.shadTheme.colorScheme.foreground,
+            return GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => DashboardPage()),
+                  (route) => false,
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: context.shadTheme.radius,
+                  border: Border.all(
+                    color: context.shadTheme.colorScheme.border,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Rumah Tangga ${index + 1}',
-                        style: context.textTheme.small,
-                      ),
-                      Text(
-                        'Rumah Tangga deskription ${index + 1}',
-                        style: context.textTheme.muted,
-                      ),
-                    ],
-                  ),
-                ],
+                ),
+                child: Row(
+                  spacing: 12,
+                  children: [
+                    Icon(
+                      LucideIcons.notebookPen,
+                      size: 24,
+                      color: context.shadTheme.colorScheme.foreground,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Rumah Tangga ${index + 1}',
+                          style: context.textTheme.small,
+                        ),
+                        Text(
+                          'Rumah Tangga deskription ${index + 1}',
+                          style: context.textTheme.muted,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             );
           },
