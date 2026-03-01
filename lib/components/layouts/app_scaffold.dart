62167484a94bf1a31cfdd70f25f1450a0100dffa
yysrll <yusril.uzumaki@gmail.com>
2026-03-01 14:33:31 +0700

    Enhance AppScaffold to support appBar and floatingActionButton

--- lib/components/layouts/app_scaffold.dart
+++ lib/components/layouts/app_scaffold.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AppScaffold extends StatelessWidget {
-  const AppScaffold({super.key, required this.body});
+  const AppScaffold({
+    super.key,
+    required this.body,
+    this.appBar,
+    this.floatingActionButton,
+  });
 
   final Widget body;
+  final PreferredSizeWidget? appBar;
+  final Widget? floatingActionButton;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(body: SafeArea(child: body)),
+      child: Scaffold(
+        appBar: appBar,
+        floatingActionButton: floatingActionButton,
+        body: SafeArea(child: body),
+      ),
     );
   }
 }
