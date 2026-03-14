b7ade5b8175af14e9a937a6966149a220bac9456
yysrll <yusril.uzumaki@gmail.com>
2026-03-14 20:43:30 +0700

    Refactor LoginPage to use KasTextFormField for email and password inputs

--- lib/features/auth/presentation/login/login_page.dart
+++ lib/features/auth/presentation/login/login_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kas_rumah/components/input/kas_text_form_field.dart';
 import 'package:kas_rumah/components/layouts/app_scaffold.dart';
 import 'package:kas_rumah/core/utils/constant/dimen_constant.dart';
 import 'package:kas_rumah/core/utils/context/context_ext.dart';
-import 'package:kas_rumah/features/auth/components/email_input.dart';
-import 'package:kas_rumah/features/auth/components/password_input.dart';
 import 'package:kas_rumah/features/workspace/presentation/list/workspace_page.dart';
 import 'package:kas_rumah/gen/assets.gen.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -42,14 +40,20 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 24),
                 Text(
                   context.strings.loginTitle,
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
                 const SizedBox(height: 24),
-                EmailInput(controller: _emailController),
+                KasTextFormField(
+                  controller: _emailController,
+                  labelText: "Email",
+                ),
                 const SizedBox(height: 16),
-                PasswordInput(controller: _passwordController),
+                KasTextFormField(
+                  controller: _passwordController,
+                  labelText: "Password",
+                ),
                 const SizedBox(height: 24),
-                ShadButton(
+                FilledButton(
                   onPressed: () {
                     if (_formKey.currentState?.validate() ?? false) {}
                     Navigator.of(context).pushAndRemoveUntil(
@@ -57,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                       (Route<dynamic> route) => false,
                     );
                   },
-                  child: Text(context.strings.loginButton),
+                  child: const Text("Masuk dengan Google"),
                 ),
               ],
             ),
